cd89487a2db0ee48fa0d38c35831c1584abf9e04
tcollins2011 <62220253+tcollins2011@users.noreply.github.com>
2022-02-15 22:20:39 -0800

    Made a product class

--- github_practice.py
+++ github_practice.py
@@ -1,2 +1,11 @@
 # This file will serve as random stuff to practice git merge conflicts and branching with.
 
+class Product():
+
+    def make_product(self):
+        print('I made a product')
+
+
+t = Product()
+
+t.make_product()
